562ca94529799bdd51f1a42fa859aecde06c3bc0
Joshua Li <joshuali925@gmail.com>
2021-08-30 11:05:07 -0700

    Fix corner cases for side nav when finding selected id
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- public/components/common/side_nav.tsx
+++ public/components/common/side_nav.tsx
@@ -13,16 +13,23 @@ import { EuiPage, EuiPageBody, EuiPageSideBar, EuiSideNav, EuiSideNavItemType } 
 import React from 'react';
 
 export const renderPageWithSidebar = (BodyComponent: React.ReactNode) => {
-  function setIsSelected(items: EuiSideNavItemType<React.ReactNode>[], hash: string) {
+  // set items.isSelected based on location.hash passed in
+  // tries to find an item.href that is a prefix of the hash, if no match will try the other way
+  function setIsSelected(
+    items: EuiSideNavItemType<React.ReactNode>[],
+    hash: string,
+    initial = true,
+    reverse = false
+  ): boolean {
     for (let i = 0; i < items.length; i++) {
       const item = items[i];
-      if (item.href === hash) {
+      if (item.href && ((reverse && item.href.startsWith(hash)) || hash.startsWith(item.href))) {
         item.isSelected = true;
         return true;
       }
-      if (item.items?.length && setIsSelected(item.items, hash)) return true;
+      if (item.items?.length && setIsSelected(item.items, hash, false, reverse)) return true;
     }
-    return false;
+    return initial && setIsSelected(items, hash, false, !reverse);
   }
 
   const items = [
