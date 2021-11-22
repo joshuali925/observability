87f8380a4f371bedb4780bd8a0a3b46c8b8db850
Joshua Li <joshuali925@gmail.com>
2021-11-22 19:01:12 +0000

    try webpack
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- dashboards-observability/.cypress/plugins/index.js
+++ dashboards-observability/.cypress/plugins/index.js
@@ -21,14 +21,88 @@
  * @type {Cypress.PluginConfig}
  */
 
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  require('@cypress/code-coverage/task')(on, config)
-  on('file:preprocessor', require('@cypress/code-coverage/use-browserify-istanbul'))
-  // include any other plugin code...
+const cypressWebpackPreprocessor = require('@cypress/webpack-preprocessor');
 
-  // It's IMPORTANT to return the config object
-  // with any changed environment variables
-  return config
+const fs = require('fs');
+
+function getAllFiles(dirPath, arrayOfFiles) {
+  const files = fs.readdirSync(dirPath);
+
+  let currentArrayOfFiles = arrayOfFiles || [];
+
+  files.forEach((file) => {
+    if (fs.statSync(`${dirPath}/${file}`).isDirectory()) {
+      currentArrayOfFiles = getAllFiles(`${dirPath}/${file}`, currentArrayOfFiles);
+    } else if (file.match(/\.tsx?$/)) {
+      currentArrayOfFiles.push(`${dirPath}/${file}`);
+      // eslint-disable-next-line no-console
+      console.log(file);
+    }
+  });
+
+  return currentArrayOfFiles;
 }
+
+function addSourcesAsAdditionalEntries() {
+  // eslint-disable-next-line no-console
+  console.log('Adding the following files as additional entries to the webpack configuration:');
+  return getAllFiles('./public/', []);
+}
+
+// These webpack options are not needed for this minimal working example
+// but are relevant for the actual project that suffers the same problem.
+const webpackOptions = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ts$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: 'css-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.d\.ts$/,
+        loader: 'ignore-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.json'],
+  },
+};
+
+const options = {
+  webpackOptions,
+  watchOptions: {},
+  additionalEntries: addSourcesAsAdditionalEntries(),
+};
+
+module.exports = (on, config) => {
+  require('@cypress/code-coverage/task')(on, config);
+  // on('file:preprocessor', require('@cypress/code-coverage/use-browserify-istanbul'))
+  // return config
+  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
+  on('file:preprocessor', cypressWebpackPreprocessor(options));
+
+  return {
+    ...config,
+    env: {
+      ...config.env,
+      coverage: true,
+    },
+  };
+};
